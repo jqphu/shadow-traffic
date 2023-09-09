4dfbf931521786032b3886c886c06d766b019a06
Justin Phu <me@justinphu.dev>
2023-09-09 16:57:25 -0700

    ✨ log if shadow response doesn't match prod response

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import shadowTraffic from './shadow';
 
 /// Env Variables Required.
 export interface Env {
-  SHADOW_BASE: string;
-  PROD_BASE: string;
+	SHADOW_BASE: string;
+	PROD_BASE: string;
 }
 
 export default {
@@ -13,26 +13,30 @@ export default {
 		const prodUrl = new URL(urlParts, env.PROD_BASE);
 
 		try {
-			const prodRequest = new Request(prodUrl, request);
+			const prodRequest = new Request(prodUrl, request.clone());
 			const shadowRequest = new Request(new URL(urlParts, env.SHADOW_BASE), request);
 
-      // Note: we need to fetch here so we can re-use the body.
-      //
-      // If we fetch elsewhere, we'll get an error reconstructing a Request with a used body.
-			let prodResponse = fetch(prodRequest);
+			// Note: we need to fetch here so we can re-use the body.
+			//
+			// If we fetch elsewhere, we'll get an error reconstructing a Request with a used body.
+			//
+			// TODO(jqphu): We need to wait for the prod request to complete. If we don't await here and try to return the response. The worker fails as it tries to read the body after the response has already been sent.
+			let prodResponse = await fetch(prodRequest);
 			let shadowResponse = fetch(shadowRequest);
 
 			// Kick this off in the background, don't block.
 			// Make sure worker is alive even though we return a response.
-			context.waitUntil(shadowTraffic(prodResponse, shadowResponse));
+			//
+			// TODO(jqphu): cloning the response here is going to be paid for every request. Probably need to benchmark this if we are sending a lot of data.
+			context.waitUntil(shadowTraffic(prodResponse.clone(), shadowResponse));
 
 			return prodResponse;
 		} catch (e) {
 			// If any error at all occurs, just redirect to the production URL.
 			//
 			// This shouldn't happen in regular operation.
-      //
-      // NOTE: If you are using cloudflare for your domain. You can just use `context.passThroughOnException()` and remove the prodUrl.
+			//
+			// NOTE: If you are using cloudflare for your domain. You can just use `context.passThroughOnException()` and remove the prodUrl.
 			return Response.redirect(prodUrl.toString(), 307);
 		}
 	},
--- src/shadow.ts
+++ src/shadow.ts
@@ -1,8 +1,31 @@
-const shadowTraffic = async (originalResponsePromise: Promise<Response>, shadowResponsePromise: Promise<Response>) => {
-  const [originalResponse, shadowResponse] = await Promise.all([originalResponsePromise, shadowResponsePromise]);
+const shadowTraffic = async (
+	originalResponse: Response,
+	shadowResponsePromise: Promise<Response>
+) => {
+	const shadowResponse = await shadowResponsePromise;
+	if (originalResponse.status != shadowResponse.status) {
+		console.warn(`Expected Status {originalResponse.status}, Shadow Found {shadowResponse.status}`);
+		return;
+	}
 
-  console.log(originalResponse, shadowResponse);
+	const contentType = originalResponse.headers.get('content-type');
+	let originalBody;
+	let shadowBody;
 
+	if (contentType == 'application/json') {
+		originalBody = JSON.stringify(await originalResponse.json());
+		shadowBody = JSON.stringify(await shadowResponse.json());
+	} else if (contentType == 'text/plain') {
+		originalBody = await originalResponse.text();
+		shadowBody = await shadowResponse.text();
+	} else {
+		// No/Unknown body.
+		return;
+	}
+
+	if (originalBody != shadowBody) {
+		console.warn(`Body returned different results: Original '{originalBody}', Shadow '{shadowBody}'`);
+	}
 };
 
 export default shadowTraffic;
